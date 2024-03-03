e778d88fd983b63c69d62b63eef9db4f6e67c84d
YewJin1010 <yewjinc@gmail.com>
2024-03-03 13:23:48 +0800

    Functional opacity and btn disable

--- frontend/src/UploadImage/UploadImage.js
+++ frontend/src/UploadImage/UploadImage.js
@@ -11,6 +11,8 @@ function UploadImage() {
   const [fileObjectsText, setFileObjectsText] = useState([]);
   const [mode, setMode] = useState('Object');
   const [showProgressBar, setShowProgressBar] = useState(false);
+  const [backgroundOpacity, setBackgroundOpacity] = useState(1);
+  const [buttonsDisabled, setButtonsDisabled] = useState(false);
  
   // Log current mode
   useEffect(() => {
@@ -95,6 +97,8 @@ function UploadImage() {
     setShowProgressBar(true);
     const progressBar = document.querySelector('.progress-bar');
     progressBar.style.width = '0%';
+    setBackgroundOpacity(0.5);
+    setButtonsDisabled(true);
 
     console.log('Submitting images');
     console.log('File objects Object:', fileObjectsObject);
@@ -184,60 +188,61 @@ function UploadImage() {
   }
 
   return (
-    <div className="background">
-      <br /><br /><br /><br /><br />
-      
+    <div>
       <div class="progress" id="progressBar" style={{ visibility: showProgressBar ? 'visible' : 'hidden' }}>
           <div class="progress-bar" role="progressbar" style={{ width: '0%' }} aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
       </div>
-      <h3 className='mode-text'>{mode} Mode</h3>
-      <div className="upload">
-        <div className="upload-wrapper">
-          <div className="upload-img">
-          {(mode === 'Object' ? previewImagesObject : previewImagesText).map((image, index) => (
-              <div key={index} className="uploaded-img">
-                <img src={image} alt={`Preview ${index}`} />
-                <button type="button" className="remove-btn" onClick={() => handleRemoveImage(index)}>
-                  <b className='remove-btn-text'>x</b>
-                </button>
-              </div>
-            ))}
-          </div>
-          <div className="upload-info">
-            <p>
-            <span className="upload-info-value">{mode === 'Object' ? previewImagesObject.length : previewImagesText.length}</span> file(s) uploaded.
-            </p>
-          </div>
-          <div className="upload-area" onClick={() => document.getElementById('upload-input').click()}>
-            <div className="upload-area-img">
-              <img src="assets/upload.png" alt="upload icon" />
-            </div>
-            <p className="upload-area-text">Select images</p>
-          </div>
-          <input type="file" className="visually-hidden" id="upload-input" multiple onChange={handleFileChange} />
-        </div>
-      </div>
-      <div className="action-buttons">
-        <div className="popup-container">
-          <button className='openPopup' onClick={openPopup}>Available Ingredients</button>
-            {isPopupOpen && (
-              <div id="popup" className="popup">
-                <div className="popup-content">
-                  <span className="close" onClick={closePopup}>&times;</span>
-                  <p className='ingre-title'>Ingredients available <br></br>for detection:</p>
-                  <ul className='ingre-items'>
-                    {ingredients.map((ingredient, index) => (
-                      <li key={index}>{ingredient}</li>
-                    ))}
-                  </ul>
+      <div className="background" style={{ opacity: backgroundOpacity }}>
+        <br /><br /><br /><br /><br />
+        <h3 className='mode-text'>{mode} Mode</h3>
+        <div className="upload">
+          <div className="upload-wrapper">
+            <div className="upload-img">
+            {(mode === 'Object' ? previewImagesObject : previewImagesText).map((image, index) => (
+                <div key={index} className="uploaded-img">
+                  <img src={image} alt={`Preview ${index}`} />
+                  <button type="button" className="remove-btn" onClick={() => handleRemoveImage(index)}>
+                    <b className='remove-btn-text'>x</b>
+                  </button>
                 </div>
+              ))}
+            </div>
+            <div className="upload-info">
+              <p>
+              <span className="upload-info-value">{mode === 'Object' ? previewImagesObject.length : previewImagesText.length}</span> file(s) uploaded.
+              </p>
+            </div>
+            <div className="upload-area" onClick={() => document.getElementById('upload-input').click()}>
+              <div className="upload-area-img">
+                <img src="assets/upload.png" alt="upload icon" />
               </div>
-            )}    
+              <p className="upload-area-text">Select images</p>
+            </div >
+            <input type="file" className="visually-hidden" id="upload-input" multiple onChange={handleFileChange} disabled={buttonsDisabled}/>
+          </div>
         </div>
-        <button className="submit-btn" onClick={submitImages}>Submit</button>       
-        <button className="info-btn">Info</button>   
-        <button className='change-btn' onClick={handleDetectChange}>Change Detector</button>
-        </div>
+        <div className="action-buttons">
+          <div className="popup-container">
+            <button className='openPopup' onClick={openPopup} disabled={buttonsDisabled}>Available Ingredients</button>
+              {isPopupOpen && (
+                <div id="popup" className="popup">
+                  <div className="popup-content">
+                    <span className="close" onClick={closePopup}>&times;</span>
+                    <p className='ingre-title'>Ingredients available <br></br>for detection:</p>
+                    <ul className='ingre-items'>
+                      {ingredients.map((ingredient, index) => (
+                        <li key={index}>{ingredient}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              )}    
+          </div>
+          <button className="submit-btn" onClick={submitImages} disabled={buttonsDisabled}>Submit</button>       
+          <button className="info-btn" disabled={buttonsDisabled}>Info</button>   
+          <button className='change-btn' onClick={handleDetectChange} disabled={buttonsDisabled}>Change Detector</button>
+          </div>
+      </div>
     </div>
   );
 }
