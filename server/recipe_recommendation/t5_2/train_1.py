fceac52276c8ac27abcc672529b86ce9051c3eb1
YJ <yewjinc@gmail.com>
2024-03-13 18:22:21 +0800

    update

--- server/recipe_recommendation/t5_2/train_1.py
+++ server/recipe_recommendation/t5_2/train_1.py
@@ -11,10 +11,10 @@ model = T5ForConditionalGeneration.from_pretrained("t5-small")
 # Example DataFrame with "a" and "b" columns
 df = pd.read_csv('server/recipe_recommendation/t5_2/new_data.csv')
 df['ingredients'] = df['ingredients'].fillna('')
-df = df[:50]  # Select first 10 rows for demonstration
+df = df[:100]  # Select first 10 rows for demonstration
 
 # Define batch size
-batch_size = 2
+batch_size = 4
 
 # Tokenize "a" and "b" columns in batches
 inputs = []
@@ -27,17 +27,22 @@ for i in range(0, len(df), batch_size):
     ingredients_tokens = tokenizer(batch_df["ingredients"].tolist(), return_tensors="pt", padding=True, truncation=True)
 
     # Tokenize "title_directions" column for inputs
-    title_directions_tokens_input = tokenizer(batch_df["directions"].tolist(), return_tensors="pt", padding=True, truncation=True)
+    #title_directions_tokens_input = tokenizer(batch_df["directions"].tolist(), return_tensors="pt", padding=True, truncation=True)
 
     # Tokenize "title_directions" column for labels
     title_directions_tokens_label = tokenizer(batch_df["directions"].tolist(), return_tensors="pt", padding=True, truncation=True)
 
     # Add tokenized inputs and labels to the list
+    """
     inputs.append({
         "input_ids": torch.cat([ingredients_tokens["input_ids"], title_directions_tokens_input["input_ids"]], dim=1),
         "attention_mask": torch.cat([ingredients_tokens["attention_mask"], title_directions_tokens_input["attention_mask"]], dim=1)
     })
-
+    """ 
+    inputs.append({
+        "input_ids": ingredients_tokens["input_ids"],
+        "attention_mask": ingredients_tokens["attention_mask"]
+    })
     labels.append({
         "input_ids": title_directions_tokens_label["input_ids"],
         "attention_mask": title_directions_tokens_label["attention_mask"]
@@ -48,7 +53,7 @@ loss = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
 # Fine-tuning loop
-num_epochs = 3
+num_epochs = 10
 for epoch in range(num_epochs):
     total_loss = 0.0
     for input_batch, label_batch in zip(inputs, labels):
@@ -77,7 +82,7 @@ tokenizer.save_pretrained(tokenizer_path)
 # Inferencing with the fine-tuned model
 input_text = "1 cup flour, 1 cup sugar, 1 egg"
 input_encodings = tokenizer(input_text, return_tensors="pt", padding=True, truncation=True)
-output_ids = model.generate(input_encodings["input_ids"], attention_mask=input_encodings["attention_mask"], max_length=64, num_beams=4)
+output_ids = model.generate(input_encodings["input_ids"], attention_mask=input_encodings["attention_mask"], max_length=500, num_beams=8)
 output_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
 print(output_text)
 
