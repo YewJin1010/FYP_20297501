5995f63d35773af6328562ae59aae52fd8437956
YJ <yewjinc@gmail.com>
2024-03-13 09:03:04 +0800

    t5 update

--- server/recipe_recommendation/t5_2/train_3.py
+++ server/recipe_recommendation/t5_2/train_3.py
@@ -1,7 +1,8 @@
 import torch
 import pandas as pd
 from transformers import T5ForConditionalGeneration, T5Tokenizer
-
+from transformers import AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, Seq2SeqTrainer
+import numpy as np
 # Load the T5 tokenizer
 tokenizer = T5Tokenizer.from_pretrained("t5-small")
 
@@ -34,6 +35,60 @@ labels = {
     "input_ids": title_directions_tokens_label["input_ids"],
     "attention_mask": title_directions_tokens_label["attention_mask"]
 }
+
+batch_size = 8
+model_dir = "server/recipe_recommendation/t5_2/model"
+args = Seq2SeqTrainingArguments(
+    model_dir,
+    evaluation_strategy="steps",
+    eval_steps=100,
+    logging_strategy="steps",
+    logging_steps=100,
+    save_strategy="steps",
+    save_steps=200,
+    learning_rate=4e-5,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    weight_decay=0.01,
+    save_total_limit=3,
+    num_train_epochs=1,
+    predict_with_generate=True,
+    fp16=True,
+    load_best_model_at_end=True,
+    metric_for_best_model="rouge1",
+    report_to="tensorboard"
+)
+
+data_collator = DataCollatorForSeq2Seq(tokenizer)
+metric = load_metric("rouge")
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+    
+    # Replace -100 in the labels as we can't decode them.
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+    
+    # Rouge expects a newline after each sentence
+    decoded_preds = ["\n".join(nltk.sent_tokenize(pred.strip()))
+                      for pred in decoded_preds]
+    decoded_labels = ["\n".join(nltk.sent_tokenize(label.strip())) 
+                      for label in decoded_labels]
+    
+    # Compute ROUGE scores
+    result = metric.compute(predictions=decoded_preds, references=decoded_labels,
+                            use_stemmer=True)
+
+    # Extract ROUGE f1 scores
+    result = {key: value.mid.fmeasure * 100 for key, value in result.items()}
+    
+    # Add mean generated length to metrics
+    prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id)
+                      for pred in predictions]
+    result["gen_len"] = np.mean(prediction_lens)
+    
+    return {k: round(v, 4) for k, v in result.items()}
 
 # Define the loss function and optimizer
 loss = torch.nn.CrossEntropyLoss()
