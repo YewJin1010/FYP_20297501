147a961ae4cbe86e77f757bb54a3e49746a251c1
YewJin1010 <yewjinc@gmail.com>
2024-03-20 07:02:28 +0800

    updated dataset

--- server/recipe_recommendation/t5/utils/create_csv.py
+++ server/recipe_recommendation/t5/utils/create_csv.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 def create_csv(csv_path):
     df = pd.read_csv(csv_path)
     # Create new csv with title, ingredients, and directions
@@ -10,15 +9,18 @@ def create_csv(csv_path):
     df['ingredients'] = df['ingredients'].str.replace('[', '').str.replace(']', '')
     df['directions'] = df['directions'].str.replace('[', '').str.replace(']', '')
 
+    # Wrap title in "" and add + after it
+    df['title'] = '"' + df['title'] + '" + '  
+
     # Combine title and directions
-    df['title_directions'] = df['title'] + ' ' + df['directions']
+    df['title_directions'] = df['title'] + df['directions']
 
     # Drop title and directions
     df = df.drop(['title', 'directions'], axis=1)
 
     # Save new csv
-    df.to_csv('server/recipe_recommendation/t5/csv/recipes_t5.csv', index=False)
+    save_path = 'server/recipe_recommendation/t5/dataset_backup/recipes_t5.csv'
+    df.to_csv(save_path, index=False)
 
-
-csv_path = 'server/recipe_recommendation/t5/csv/recipes.csv'
+csv_path = 'server/recipe_recommendation/t5/dataset_backup/recipes.csv'
 create_csv(csv_path)
